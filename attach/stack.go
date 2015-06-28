7b8306150bf1484ce3360de5793f2714277139fe
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-06-29 08:04:52 +0900

    parse thread details

--- attach/stack.go
+++ attach/stack.go
@@ -2,6 +2,7 @@ package attach
 
 import (
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -13,11 +14,13 @@ type JavaThread struct {
 
 type StackItem struct {
 	method string
+	file   string
+	line   int
 }
 
 var header_re = regexp.MustCompile(`^"([^"]+)"`)
 var thread_state_re = regexp.MustCompile(`java.lang.Thread.State: ([A-Z_]+)(?: \((.*)\))?`)
-var stack_at_re = regexp.MustCompile(`\s*at ([^(]+)\(`)
+var stack_at_re = regexp.MustCompile(`\s*at ([^(]+)\((.*?)(?::(\d+))?\)`)
 
 func ParseStack(stack string) ([]*JavaThread, error) {
 
@@ -32,7 +35,19 @@ func ParseStack(stack string) ([]*JavaThread, error) {
 		} else if matches = thread_state_re.FindStringSubmatch(line); len(matches) >= 2 {
 			state = matches[1]
 		} else if matches = stack_at_re.FindStringSubmatch(line); len(matches) >= 1 {
-			stacks = append(stacks, &StackItem{matches[1]})
+			file := matches[2]
+			var line int
+			if len(matches) == 4 {
+				l, err := strconv.Atoi(matches[3])
+				if err != nil {
+					line = -1
+				} else {
+					line = l
+				}
+			} else {
+				line = -1
+			}
+			stacks = append(stacks, &StackItem{matches[1], file, line})
 		} else if len(name) > 0 {
 			threads = append(threads, &JavaThread{name, state, stacks})
 			name = ""
